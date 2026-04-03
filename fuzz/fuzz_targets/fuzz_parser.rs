8c4482ef58c76bcaf9a012be2de51ad66924898a
Noriyuki Kurosu <3p0rta26@gmail.com>
2026-04-04 04:32:31 +0900

    test: add cargo-fuzz setup with parser fuzz target

---
+++ fuzz/fuzz_targets/fuzz_parser.rs
@@ -0,0 +1,8 @@
+#![no_main]
+use libfuzzer_sys::fuzz_target;
+
+fuzz_target!(|data: &[u8]| {
+    if let Ok(input) = std::str::from_utf8(data) {
+        let _ = wirespec_syntax::parse(input);
+    }
+});
